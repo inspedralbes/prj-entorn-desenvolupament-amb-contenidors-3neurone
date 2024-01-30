75985388d26d80af4885c3d5a28e41367cfd37c7
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2024-01-30 11:55:37 +0100

    Revert "FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAA"
    This reverts commit d25c25abe6a8fccd81ec7e32e70444d439adaaf6.

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,13 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    vite: {
-        server:{
-          host: "0.0.0.0",
-          hmr: { clientPort: 4321 },
-          port: 4321, 
-          watch: { usePolling: true }
-        }
-      }
-});
+export default defineConfig({});
